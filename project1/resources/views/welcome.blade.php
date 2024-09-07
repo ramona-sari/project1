62f71f9469d4873d59b568c77f5a8eb8d552028d
DwiPitriyani <pitriyanidwi92@gmail.com>
2024-09-07 12:39:27 +0700

    first commit

---
+++ project1/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Beranda</title>
+</head>
+<body>
+    <h3>Saya Baru Belajar Framework Laravel!</h3>
+</body>
+</html>
