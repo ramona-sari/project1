769ff74e5896e319fa8d5d3460b4dc73025de998
ramona-sari <ramonasari04@gmail.com>
2024-10-24 10:27:12 +0700

    Update AppServiceProvider.php

--- project2/app/Providers/AppServiceProvider.php
+++ project2/app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Paginator::useBootstrap();
     }
 }
+
